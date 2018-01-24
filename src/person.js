6db98d846cd534d1c1cd74a6f44cef9cd24b72a5
Allan <allansp9@gmail.com>
2018-01-24 17:04:28 -0200

    One Component per File

--- src/person.js
+++
@@ -1,4 +0,0 @@
-export const isAdult = a => a >= 18;
-export const canDrink = a => a >= 21;
-
-export default age => age >= 65;
--- src/utils.js
+++
@@ -1,7 +0,0 @@
-const square = x => x * x;
-
-const add = (a, b) => a + b;
-
-const subtract = (a, b) => a - b;
-
-export { add, square, subtract as default };
